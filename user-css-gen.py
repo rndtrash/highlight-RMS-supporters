2a6ebf43b7c81ddaca5cb54f3b8a96016a37e05e
Ivan Kuzmenko <6745157+rndtrash@users.noreply.github.com>
2021-03-31 16:29:19 +0300

    Add tlani to authors, version 3.0.0, split selector every 500 users

--- user-css-gen.py
+++ user-css-gen.py
@@ -1,29 +1,44 @@
 #!/usr/bin/env python3
 
+import math
+
+SPLIT_EVERY = 500
+
 def make_css(text, color):
 	return '{' + 'content: "{0}";display: inline-block;margin-left: 4px;padding: 0 7px;font-size: 12px;font-weight: 500;line-height: 18px;border-radius: 2em;border: 1px solid transparent;color: var(--color-pr-state-{1}-text);background-color: var(--color-pr-state-{1}-bg);border-color: var(--color-pr-state-{1}-border);'.format(text, color) + '}' # thx python
 
 def add_entries_to_css(f, file, text, color):
 	f.write('@-moz-document domain("github.com") {\n')
 	with open(file + '-gh.txt') as f_gh:
-		first_entry = True
-		for l in f_gh.read().splitlines():
-			if first_entry:
-				first_entry = False
-			else:
-				f.write(',')
-			f.write('a[href="/{}"]:after'.format(l)) # [data-hovercard-type="user"]
-		f.write(make_css(text, color) + '\n')
+		lines = f_gh.read().splitlines()
+		# TODO: smells like shitcode
+		for i in range(int(math.ceil(len(lines) / SPLIT_EVERY)) + 1): # Make new selector every 500 users
+			lines_split = lines[i * SPLIT_EVERY:(i + 1) * SPLIT_EVERY]
+			if len(lines_split) == 0:
+				break
+			first_entry = True
+			for l in lines_split:
+				if first_entry:
+					first_entry = False
+				else:
+					f.write(',')
+				f.write('a[href="/{}"]:after'.format(l)) # [data-hovercard-type="user"]
+			f.write(make_css(text, color) + '\n')
 	f.write('}\n')
 	with open(file + '-global.txt') as f_global:
-		first_entry = True
-		for l in f_global.read().splitlines():
-			if first_entry:
-				first_entry = False
-			else:
-				f.write(',')
-			f.write('a[href="{}"]:after'.format(l))
-		f.write(make_css(text, color) + '\n')
+		lines = f_global.read().splitlines()
+		for i in range(int(math.ceil(len(lines) / SPLIT_EVERY)) + 1):
+			lines_split = lines[i * SPLIT_EVERY:(i + 1) * SPLIT_EVERY]
+			if len(lines_split) == 0:
+				break
+			first_entry = True
+			for l in lines_split:
+				if first_entry:
+					first_entry = False
+				else:
+					f.write(',')
+				f.write('a[href="{}"]:after'.format(l))
+			f.write(make_css(text, color) + '\n')
 
 with open('rms-supporters-haters-highlighter.user.css', 'w') as f:
 	f.write(
@@ -31,9 +46,9 @@ with open('rms-supporters-haters-highlighter.user.css', 'w') as f:
 /* ==UserStyle==
 @name           RMS supporters/haters highlighter
 @namespace      https://github.com/rndtrash/rms-supporters-haters-highlighter
-@version        2.0.0
+@version        3.0.0
 @description    nuff said
-@author         rndtrash and frens :)
+@author         rndtrash, tlani and frens :)
 ==/UserStyle== */
 """ + '\n'
 )
